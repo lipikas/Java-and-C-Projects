2c542831c82b31f0875b50bb54e6f4c3893aa5de
lipikas <72999725+lipikas@users.noreply.github.com>
2020-10-18 16:12:34 -0400

    Update determinant.c

--- C/Determinant/determinant.c
+++ C/Determinant/determinant.c
@@ -32,40 +32,37 @@ int main (int argc, char* argv[argc+1]){
     free(matrix);
     printf("%d\n", result);
 	return EXIT_SUCCESS;
-};
+}
 int det (int** mat, int n){ // finds determinant
     if (n == 1) return mat[0][0];
     return mat[0][0]*mat[1][1] - mat[0][1]*mat[1][0];
-};
+}
+void freen(int** matrix, int n, int count){
+    if(count == n)  return;
+    free(matrix[count]);
+    freen(matrix, n, ++count);
+}
+int creat(int** matrix, int** mat, int n, int c, int row, int col, int x, int y){
+    if(col >= n-1) return 0;
+    if(row >= n-1) return 1;
+    //if (col == 0) y = c;
+    if(col == 0 && c!=0) y =0;
+    if (y == c) y++;
+    mat[row][col] = matrix[x][y];
+    if(creat(matrix, mat, n, c, row, ++col, x, ++y) ==1) return 1;
+    return creat(matrix, mat, n, c, ++row, 0, ++x, c);
+
+}
 int rec (int** matrix, int n){ //  finds determinant via recursion
     if (n==2)  return det(matrix, n);
     int result =0, a = 1; int** mat = malloc(n*sizeof(int*));
     for (int j = 0; j < n; j ++)  mat[j] = malloc(n*sizeof(int));
     for (int i  = 0; i < n; i++){
-        creat(matrix, mat, n, i); // creates submatrix
+        creat(matrix, mat, n, i, 0, 0, 1, i); // creates submatrix
         result = result+ rec(mat, n-1)*a*matrix[0][i];
         a *= -1;
     }
     freen(mat, n, 0);
     free(mat);
     return result;
-};
-void freen(int** matrix, int n, int count){
-    if(count == n)  return;
-    free(matrix[count]);
-    freen(matrix, n, ++count);
-};
-void creat(int** matrix, int** mat, int n, int c){ //; intilaizes submatrixes
-    int row =1;
-    for (int y =0; y < n-1; y++){
-        int col = c;
-        if (c!=0) col =0;
-        for (int z = 0; z < n-1; z++){  
-            if (col == c)   col++;
-			mat[y][z] = matrix[row][col];
-            //printf("%d ", mat[y][z]);
-            col++;
-        }
-        row++;
-    }
-};
+}
