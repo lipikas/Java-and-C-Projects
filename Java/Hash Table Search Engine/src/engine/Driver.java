b21bced94fe9bce448b4f31ab0deaddd6b5afff4
lipikas <72999725+lipikas@users.noreply.github.com>
2021-01-20 19:11:00 -0500

    Update Driver.java

--- Java/Hash Table Search Engine/src/engine/Driver.java
+++ Java/Hash Table Search Engine/src/engine/Driver.java
@@ -1,49 +1,24 @@
 package engine;
-import java.io.*;
 import java.util.*;
 public class Driver {
 
-	public static void main(String[] args) throws FileNotFoundException {
-
-		ArrayList<Frequency> p = new ArrayList<Frequency>();
+	public static void main(String[] args) {
+        HashEngine f = new HashEngine();
+		ArrayList<Frequency> p = new ArrayList<>();
 		String u= "a";
 		for (int i = 1; i < 26; i ++) {
-		u = u+u;
+            u = u+u;
 
-		if (i == 25) {
-			Frequency b = new Frequency(u, 0);
-			p.add(b);
+            if (i == 25) {
+                Frequency b = new Frequency(u, 0);
+                p.add(b);
+            }
+            else {
+                Frequency b = new Frequency(u, i+1);
+                p.add(b);
+            }
 		}
-		else {
-			Frequency b = new Frequency(u, i+1);
-			p.add(b);
-		}
-
-		}
-		ArrayList<Integer> o = f.insertLast(p);
+		ArrayList<Integer> o = f.insertEnd(p);
 		System.out.println(o+" ");
-
 	}
 }
-	/*public void printGraph(){
-		System.out.println(people.size());
-		for(int v = 0; v < people.size(); v++){
-			if(people.get(v).school != null){
-				System.out.println(people.get(v).name + "|y|" + people.get(v).school);
-			}
-			else{
-				System.out.println(people.get(v).name + "|n");
-			}
-		}
-		for(int v = 0; v < people.size(); v++){
-			Network ptr = people.get(v).networks;
-			while(ptr != null){
-				if(v < ptr.index){
-					System.out.println(people.get(v).name + "|" + nameForIndex(ptr.index));
-				}
-				ptr = ptr.next;
-			}
-			
-		}
-	}
-}*/
