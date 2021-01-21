a050b050ed74b2a0830a0e524b6482fa4931951e
lipikas <72999725+lipikas@users.noreply.github.com>
2021-01-20 19:27:47 -0500

    Update Driver.java

--- Java/Network Evaluator/src/network/Driver.java
+++ Java/Network Evaluator/src/network/Driver.java
@@ -1,1 +1,15 @@
+package network;
+import java.io.*;
+import java.util.Scanner;
 
+public class Driver {
+	public static void main(String[] args){
+		Evaluator g=null;
+		try{
+			g=new Evaluator(new Scanner(new File("1.txt")));
+		}catch(FileNotFoundException e){
+			System.out.println("File not found");
+		}
+		System.out.println(Networks.connectors(g));
+	}
+}
