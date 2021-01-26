2b70d197157644e31d01dbcea20ce26367d1b4c6
Lipika S <72999725+lipikas@users.noreply.github.com>
2021-01-26 12:20:15 -0500

    Update Driver.java

--- Java/Hash Map Search Engine/src/engine/Driver.java
+++ Java/Hash Map Search Engine/src/engine/Driver.java
@@ -3,20 +3,19 @@ import java.util.*;
 public class Driver {
 
 	public static void main(String[] args) {
-        HashEngine f = new HashEngine();
+        	HashEngine f = new HashEngine();
 		ArrayList<Frequency> p = new ArrayList<>();
-		String u= "a";
+		String namr= "a";
 		for (int i = 1; i < 26; i ++) {
-            u = u+u;
-
-            if (i == 25) {
-                Frequency b = new Frequency(u, 0);
-                p.add(b);
-            }
-            else {
-                Frequency b = new Frequency(u, i+1);
-                p.add(b);
-            }
+			name += name;
+			if (i == 25) {
+				Frequency b = new Frequency(u, 0);
+				p.add(b);
+			}
+			else {
+				Frequency b = new Frequency(u, i+1);
+				p.add(b);
+		    	}
 		}
 		ArrayList<Integer> o = f.insertEnd(p);
 		System.out.println(o+" ");
