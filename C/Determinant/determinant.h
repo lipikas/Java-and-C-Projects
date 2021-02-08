58095f6694490e691a474e4895bf56637b17261a
Lipika S <72999725+lipikas@users.noreply.github.com>
2021-02-08 17:06:45 -0500

    Delete C/Determinant directory

--- C/Determinant/determinant.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DETERMINANT_H
-#define __DETERMINANT_H
-
-int rec(int**, int);
-int det (int**, int );
-void freen(int**, int, int);
-int creat(int **, int**, int, int, int, int, int, int);
-#endif
